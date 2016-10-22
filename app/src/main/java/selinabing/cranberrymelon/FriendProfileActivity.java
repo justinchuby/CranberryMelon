2510f6a699d436588474f1c40878837fd81d4002
Selina Bing <selinabing@Selinas-MacBook-Pro.local>
2016-10-22 19:37:55 -0400

    bug fixes

--- app/src/main/java/selinabing/cranberrymelon/FriendProfileActivity.java
+++ app/src/main/java/selinabing/cranberrymelon/FriendProfileActivity.java
@@ -3,6 +3,7 @@ package selinabing.cranberrymelon;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -11,14 +12,15 @@ import butterknife.ButterKnife;
 
 public class FriendProfileActivity extends AppCompatActivity {
 
-    @BindView(R.id.avatar_imageview)
-    ImageView ivAvatar;
-    @BindView(R.id.title_textview)
-    TextView tvFriendName;
-    @BindView(R.id.subtitle_textview)
-    TextView tvFriendLocation;
-    @BindView(R.id.tvTime) TextView tvRelativeTime;
-    @BindView(R.id.tvTimeOfDay) TextView tvPreciseTime;
+    @BindView(R.id.tvNumber1)
+    TextView tvPhone;
+    @BindView(R.id.tvNumber3) TextView tvEmail;
+    @BindView(R.id.tvNumber5) TextView tvLocation;
+    @BindView(R.id.LinkMessenger) ImageView ivMessenger;
+    @BindView(R.id.LinkSkype) ImageView ivSkype;
+    @BindView(R.id.LinkPhoneCall) ImageView ivPhone;
+    @BindView(R.id.LinkMessage) ImageView ivMessage;
+    @BindView(R.id.LinkFB) ImageView ivFB;
 
 
     @Override
@@ -31,7 +33,13 @@ public class FriendProfileActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        
+        // set onclicklisteners
+        ivMessage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //link to send message
+            }
+        });
 
     }
 
